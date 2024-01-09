ee085437ee8b0c4aca95937f2c18e8a3285a103e
Cody <cody.tian@icloud.com>
2024-01-09 07:20:23 -0800

    Move board to right of screen, display party composition

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ let variableWidthFont
 let instructions
 let debugCorner /* output debug text in the bottom left corner of the canvas */
 
-let posX = 300
+let posX = 700
 let posY = 300
 
 
@@ -22,7 +22,7 @@ function preload() {
 
 
 function setup() {
-    let cnv = createCanvas(600, 600)
+    let cnv = createCanvas(1000, 600)
     cnv.parent('#canvas')
     colorMode(HSB, 360, 100, 100, 100)
     textFont(font, 14)
@@ -37,38 +37,64 @@ function setup() {
 
 
 function draw() {
-    background(0, 80, 75)
+    background(234, 34, 24)
 
+
+    fill(0, 80, 75)
+    rect(400, 0, 600, 600)
     fill(20, 50, 40)
     noStroke()
-    rect(20, 20, 560, 560)
+    rect(420, 20, 560, 560)
     for (let xIncrements = 0; xIncrements < 8; xIncrements++) {
         for (let yIncrements = 0; yIncrements < 8; yIncrements++) {
             if ((xIncrements + yIncrements) % 2 === 0) {
                 fill(20, 50, 38)
-                rect(21 + xIncrements*70, 21 + yIncrements*70, 68, 68)
+                rect(421 + xIncrements*70, 21 + yIncrements*70, 68, 68)
             }
         }
         stroke(0, 0, 0)
         strokeWeight(1)
-        line(20 + xIncrements*70, 20, 20 + xIncrements*70, 580)
-        line(20, 20 + xIncrements*70, 580, 20 + xIncrements*70)
+        line(420 + xIncrements*70, 20, 420 + xIncrements*70, 580)
+        line(420, 20 + xIncrements*70, 980, 20 + xIncrements*70)
         noStroke()
     }
     stroke(0, 0, 0)
     strokeWeight(1)
-    line(580, 20, 580, 580)
-    line(20, 580, 580, 580)
+    line(980, 20, 980, 580)
+    line(420, 580, 980, 580)
 
     stroke(60, 70, 60)
     fill(10, 70, 60)
     strokeWeight(2)
-    rect(posX - 15, posY - 15, 30, 30)
+    rect(posX - 15, posY - 15, 30, 30) // display you in your position
 
-    if ((keyIsDown(65) || keyIsDown(37)) && posX > 16) posX -= 2 // A or ← = left
+    // now display the party
+    rect(10, 60, 40, 40)
+    rect(10, 110, 40, 40)
+    fill(120, 70, 40)
+    rect(10, 160, 40, 40)
+    fill(240, 70, 40)
+    rect(10, 210, 40, 40)
+
+    fill(0, 0, 100)
+    textSize(30)
+    noStroke()
+    text("Party composition:", 5, 25)
+    textSize(20)
+    text("You", 13, 87)
+    textSize(25)
+    text("1", posX - 7, posY + 8)
+    textSize(30)
+    text("#1", 55, 90)
+    text("#2", 55, 140)
+    text("#3", 55, 190)
+    text("#4", 55, 240)
+
+    if ((keyIsDown(65) || keyIsDown(37)) && posX > 416) posX -= 2 // A or ← = left
     if ((keyIsDown(87) || keyIsDown(38)) && posY > 16) posY -= 2 // W or ↑ = up
-    if ((keyIsDown(68) || keyIsDown(39)) && posX < width - 16) posX += 2 // D or → = right
-    if ((keyIsDown(83) || keyIsDown(40)) && posY < height - 16) posY += 2 // S or ↓ = down
+    if ((keyIsDown(68) || keyIsDown(39)) && posX < 984) posX += 2 // D or → = right
+    if ((keyIsDown(83) || keyIsDown(40)) && posY < 584) posY += 2 // S or ↓ = down
+
 
     /* debugCorner needs to be last so its z-index is highest */
     debugCorner.setText(`frameCount: ${frameCount}`, 2)
